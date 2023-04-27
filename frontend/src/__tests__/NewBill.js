a0ce8b9e59c7a0228f0fa82fe6c1f323dd77ca01
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2023-04-28 01:35:28 +0200

    7. NewBill test unitaires

--- frontend/src/__tests__/NewBill.js
+++ frontend/src/__tests__/NewBill.js
@@ -2,16 +2,37 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import router from "../app/Router.js";
+
+jest.mock("../app/Store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
-      //to-do write assertion
+    test("Then mail icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const mailIcon = screen.getByTestId("icon-mail");
+      expect(mailIcon).toBeTruthy();
     });
+
+    // test("Then ...", () => {
+    //   const html = NewBillUI();
+    //   document.body.innerHTML = html;
+    //   //to-do write assertion
+    // });
   });
 });
